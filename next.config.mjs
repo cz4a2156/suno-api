2c18aa11d7b60306596e4decfe9049158a62764f
cz4a2156 <cz4a2156pc@gmail.com>
2026-02-14 17:03:30 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,16 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(ttf|html)$/i,
-      type: 'asset/resource'
+      type: "asset/resource",
     });
     return config;
   },
-  experimental: {
-    serverMinification: false, // the server minification unfortunately breaks the selector class names
-  },
-};  
 
-// ✅ ここを追加
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
   experimental: {
-    serverMinification: false, // the server minification unfortunately breaks the selector class names
+    serverMinification: false,
   },
 };
 
